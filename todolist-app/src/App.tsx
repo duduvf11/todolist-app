09ea3d1d67d4f600239789c33fd385e31590d1ef
Eduardo Viegas Francisco <duduviegasfrancisco11@gmail.com>
2025-06-02 16:14:22 -0300

    chore: setup inicial com Tailwind

---
+++ todolist-app/src/App.tsx
@@ -0,0 +1,12 @@
+import { } from 'react'
+
+function App() {
+
+  return (
+      <div className="flex justify-center items-center h-screen border-1 rounded-40 bg-[#67aacb] bg-no-repeat bg-center">
+        <h1 className="text-9xl font-bold text-[#f2f3f4] text-center">Testando Tailwind, estamos muito bem!</h1>
+      </div>
+  )
+}
+
+export default App
